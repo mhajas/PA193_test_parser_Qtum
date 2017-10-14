cdeb0e2e99f383f07d2d0b12056bb95d25b079b1
mitko501 <422190@mail.muni.cz>
2017-10-14 17:30:52 +0200

    Use static cast instead of reinterpret cast

--- parser/entities/block.cpp
+++ parser/entities/block.cpp
@@ -20,9 +20,9 @@ namespace parsing_utils {
         return bint.c[0] == 1;
     }
 
-    RETURN_VALUE parse_bytes(std::istream& is, char* storage, size_t number_of_bytes, bool is_big_endian) {
+    RETURN_VALUE parse_bytes(std::istream& is, void* storage, size_t number_of_bytes, bool is_big_endian) {
         if (!is_big_endian) {
-            is.read(storage, number_of_bytes);
+            is.read(static_cast<char*>(storage), number_of_bytes);
 
             if (!is || is.gcount() != number_of_bytes) {
                 std::cout << "Failed to read " << number_of_bytes << "bytes. Read only: " << is.gcount() << std::endl;
@@ -41,12 +41,12 @@ namespace parsing_utils {
         }
 
         std::reverse(std::begin(buffer), std::end(buffer));
-        std::swap_ranges(std::begin(buffer), std::end(buffer), storage);
+        std::swap_ranges(std::begin(buffer), std::end(buffer), static_cast<char*>(storage));
 
         return SUCCESS;
     }
 
-    RETURN_VALUE parse_reverse_bytes(std::istream& is, char* storage, size_t number_of_bytes, bool is_big_endian) {
+    RETURN_VALUE parse_reverse_bytes(std::istream& is, void* storage, size_t number_of_bytes, bool is_big_endian) {
         return parse_bytes(is, storage, number_of_bytes, !is_big_endian);
     }
 }
@@ -61,19 +61,19 @@ std::ostream& operator<<(std::ostream& os, const block& block1) {
 }
 
 std::istream& operator>>(std::istream& is, block& block1) {
-    if (parsing_utils::parse_bytes(is, reinterpret_cast<char*>(&block1._version), sizeof(block1._version), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
+    if (parsing_utils::parse_bytes(is, static_cast<void*>(&block1._version), sizeof(block1._version), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
         std::cout << "Failed to read Version" << std::endl;
         is.setstate(std::ios::failbit);
         return is;
     }
 
-    if (parsing_utils::parse_reverse_bytes(is, reinterpret_cast<char*>(block1._hash_prev_block.data()), block1._hash_prev_block.size(), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
+    if (parsing_utils::parse_reverse_bytes(is, static_cast<void*>(block1._hash_prev_block.data()), block1._hash_prev_block.size(), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
         std::cout << "Failed to read hash of previous block" << std::endl;
         is.setstate(std::ios::failbit);
         return is;
     }
 
-    if (parsing_utils::parse_reverse_bytes(is, reinterpret_cast<char*>(block1._hash_merkle_root.data()), block1._hash_merkle_root.size(), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
+    if (parsing_utils::parse_reverse_bytes(is, static_cast<void*>(block1._hash_merkle_root.data()), block1._hash_merkle_root.size(), parsing_utils::is_big_endian()) != parsing_utils::SUCCESS) {
         std::cout << "Failed to read merkle root hash" << std::endl;
         is.setstate(std::ios::failbit);
         return is;
