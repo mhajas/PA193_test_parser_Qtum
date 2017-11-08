fccd1a53e93e6dbeea8e8b0392cb3185b11c89aa
Svetlovská Lenka <Lenka.Svetlovska@ysoft.com>
2017-11-08 11:12:35 +0100

    some json tests

--- testsuite/parse_test_json.cpp
+++ testsuite/parse_test_json.cpp
@@ -67,15 +67,21 @@ void compare_bin_json(int block_number) {
     EXPECT_EQ(b.compute_hash(), to_hash_type(hex_reduction(j.at("hash")))) << "Hashes are different for block: " << block_number;
     EXPECT_EQ(b.get_version(), j.at("version"));
     EXPECT_EQ(b.get_hash_merkle_root(), to_hash_type(hex_reduction(j.at("merkleroot")))) << "Parsed merkle root is different for block: " << block_number;
-    //EXPECT_EQ(b.compute_merkle_root(), (hex_reduction(j.at("merkleroot")))) << "Computed merkle root is different for block: " << block_number;
+    //wrong computed merkle root for 22383, 22385, 22389
+    //EXPECT_EQ(b.compute_merkle_root(), to_hash_type(hex_reduction(j.at("merkleroot")))) << "Computed merkle root is different for block: " << block_number;
     EXPECT_EQ(b.get_hash_state_root(), to_hash_type(hex_reduction(j.at("hashStateRoot")))) << "HashStateRoot is different for block " << block_number;
     EXPECT_EQ(b.get_hash_UTXO_root(), to_hash_type(hex_reduction(j.at("hashUTXORoot")))) << "HashUTXORoot is different for block " << block_number;
 
     EXPECT_EQ(b.get_n_time(), j.at("time")) << "Time is different for block " << block_number;
     EXPECT_EQ(b.get_n_nonce(), j.at("nonce")) << "Nonce is different for block " << block_number;
     //EXPECT_EQ(b.get_n_bits(), j.at("bits")); - convert
+    EXPECT_EQ(b.get_block_height(), j.at("height")) << "Block height is different for block " << block_number;
+
     EXPECT_EQ(b.get_hash_prev_block(), to_hash_type(hex_reduction(j.at("previousblockhash")))) << "HashPrevBlock is different for block " << block_number;
-    //EXPECT_EQ(b.get_vch_block_sig(), to_vector_type(hex_reduction(j.at("signature")))); - add REVERSE!
+
+    auto rev_hash = to_vector_type(hex_reduction(j.at("signature")));
+    std::reverse(rev_hash.begin(), rev_hash.end());
+    EXPECT_EQ(b.get_vch_block_sig(), rev_hash); //- add REVERSE!
 
     //test first transaction
     EXPECT_EQ(b.get_ft_version(), j.at("tx")[0].at("version")) << "First transaction version is different for block " << block_number;
@@ -92,11 +98,10 @@ void compare_bin_json(int block_number) {
     //?
 
     //the others transactions
-    if (b.get_number_of_transactions() > 2) {
+    if (b.get_number_of_transactions() > 1) {
         std::vector<transaction> ts = b.get_transactions();
-        int pos = 2;
-        __gnu_cxx::__normal_iterator<transaction *, std::vector<transaction>> t;
-        for (t = ts.begin() + 1; t < ts.end(); t++) {
+        int pos = 1;
+        for (auto t = ts.begin(); t < ts.end(); t++) {
             auto j_tx = j.at("tx")[pos];
             EXPECT_EQ(t.base()->get_version(), j_tx.at("version")) << "Transaction (" << pos << ") version is diffrent for block " << block_number;
             EXPECT_EQ(t.base()->get_lock_time(), j_tx.at("locktime")) << "Transaction (" << pos << ") lockTime is diffrent for block " << block_number;
@@ -106,6 +111,15 @@ void compare_bin_json(int block_number) {
             for(auto vin = vins.begin(); vin < vins.end(); vin++) {
                 EXPECT_EQ(vin.base()->_sequence, j.at("tx")[pos].at("vin")[pos_v].at("sequence"))
                                     << "Transaction (" << pos << ") vin (" << pos_v << ") is diffrent for block " << block_number;
+
+                EXPECT_EQ(vin.base()->_prevout.get_index_n(), j.at("tx")[pos].at("vin")[pos_v].at("vout"))
+                                    << "Transaction (" << pos << ") vin (" << pos_v << ") index n is diffrent for block " << block_number;
+                EXPECT_EQ(vin.base()->_prevout.get_hash(), to_hash_type(hex_reduction(j.at("tx")[pos].at("vin")[pos_v].at("txid"))))
+                                    << "Transaction (" << pos << ") vin (" << pos_v << ") txid (hash) is diffrent for block " << block_number;
+//asm in json contains flags
+//                EXPECT_EQ(vin.base()->_pub_key_script._storage, to_vector_type(hex_reduction(j.at("tx")[pos].at("vin")[pos_v].at("scriptSig").at("asm"))))
+//                                    << "Transaction (" << pos << ") vin (" << pos_v << ") scriptSig asm (script storage) is diffrent for block " << block_number;
+
                 pos_v++;
             }
             EXPECT_EQ(pos_v, t.base()->get_vin_count()) << "Transaction (" << pos << ") vin count is diffrent for block " << block_number;
