fd127f0e7b57d9c2ee9d12059027a426d03aba3a
Svetlovská Lenka <Lenka.Svetlovska@ysoft.com>
2017-11-08 09:36:18 +0100

    some json tests

--- testsuite/parse_test_json.cpp
+++ testsuite/parse_test_json.cpp
@@ -33,7 +33,7 @@ inline std::string hex_reduction(std::string &&str) {
 }
 
 template <typename T>
-inline hash_type to_array(T&& str) {
+inline hash_type to_hash_type(T&& str) {
     assert(str.size() == 32);
 
     hash_type a;
@@ -42,8 +42,15 @@ inline hash_type to_array(T&& str) {
     return a;
 }
 
+template <typename S>
+inline vector_type to_vector_type(S&& str) {
+    vector_type a(str.begin(), str.end());
+    std::copy(std::begin(str), std::end(str), a.data());
+
+    return a;
+}
+
 void compare_bin_json(int block_number) {
-    //read bin file
     std::ifstream block_bin_file("resources/test-resources/blocks/" + std::to_string(block_number) + ".bin", std::ios::binary);
     if (!block_bin_file) {
         FAIL() << "Can't open binary file: " << std::to_string(block_number) << ".bin";
@@ -57,14 +64,107 @@ void compare_bin_json(int block_number) {
     json j = json::parse(json_file);
 
 
-    EXPECT_EQ(b.compute_hash(), to_array(hex_reduction(j.at("hash")))) << "Hashes are different for block: " << block_number;
+    EXPECT_EQ(b.compute_hash(), to_hash_type(hex_reduction(j.at("hash")))) << "Hashes are different for block: " << block_number;
     EXPECT_EQ(b.get_version(), j.at("version"));
-    EXPECT_EQ(b.get_hash_merkle_root(), to_array(hex_reduction(j.at("merkleroot")))) << "Parsed merkle root is different for block: " << block_number;
-    EXPECT_EQ(b.compute_merkle_root(), to_array(hex_reduction(j.at("merkleroot")))) << "Computed merkle root is different for block: " << block_number;
-    EXPECT_EQ(b.get_hash_state_root(), to_array(hex_reduction(j.at("hashStateRoot")))) << "HashStateRoot is different for block " << block_number;
-    EXPECT_EQ(b.get_hash_UTXO_root(), to_array(hex_reduction(j.at("hashUTXORoot")))) << "HashStateRoot is different for block " << block_number;
+    EXPECT_EQ(b.get_hash_merkle_root(), to_hash_type(hex_reduction(j.at("merkleroot")))) << "Parsed merkle root is different for block: " << block_number;
+    //EXPECT_EQ(b.compute_merkle_root(), (hex_reduction(j.at("merkleroot")))) << "Computed merkle root is different for block: " << block_number;
+    EXPECT_EQ(b.get_hash_state_root(), to_hash_type(hex_reduction(j.at("hashStateRoot")))) << "HashStateRoot is different for block " << block_number;
+    EXPECT_EQ(b.get_hash_UTXO_root(), to_hash_type(hex_reduction(j.at("hashUTXORoot")))) << "HashUTXORoot is different for block " << block_number;
+
+    EXPECT_EQ(b.get_n_time(), j.at("time")) << "Time is different for block " << block_number;
+    EXPECT_EQ(b.get_n_nonce(), j.at("nonce")) << "Nonce is different for block " << block_number;
+    //EXPECT_EQ(b.get_n_bits(), j.at("bits")); - convert
+    EXPECT_EQ(b.get_hash_prev_block(), to_hash_type(hex_reduction(j.at("previousblockhash")))) << "HashPrevBlock is different for block " << block_number;
+    //EXPECT_EQ(b.get_vch_block_sig(), to_vector_type(hex_reduction(j.at("signature")))); - add REVERSE!
+
+    //test first transaction
+    EXPECT_EQ(b.get_ft_version(), j.at("tx")[0].at("version")) << "First transaction version is different for block " << block_number;
+    EXPECT_EQ(b.get_ft_lock_time(), j.at("tx")[0].at("locktime")) << "First transaction lockTime is different for block " << block_number;
+
+    auto ft_outs = b.get_ft_ctxouts();
+    int pos_ft_o = 0;
+    for(auto fout = ft_outs.begin(); fout < ft_outs.end(); fout++) {
+//                //convert to int64_t
+//                EXPECT_EQ(fout.base()->_amount, j.at("tx")[0].at("vout")[pos_ft_o].at("n"));
+        pos_ft_o++;
+    }
+    EXPECT_EQ(pos_ft_o, b.get_ft_ctxout_number()) << "First transaction vout count is diffrent for block " << block_number;
+    //?
+
+    //the others transactions
+    if (b.get_number_of_transactions() > 2) {
+        std::vector<transaction> ts = b.get_transactions();
+        int pos = 2;
+        __gnu_cxx::__normal_iterator<transaction *, std::vector<transaction>> t;
+        for (t = ts.begin() + 1; t < ts.end(); t++) {
+            auto j_tx = j.at("tx")[pos];
+            EXPECT_EQ(t.base()->get_version(), j_tx.at("version")) << "Transaction (" << pos << ") version is diffrent for block " << block_number;
+            EXPECT_EQ(t.base()->get_lock_time(), j_tx.at("locktime")) << "Transaction (" << pos << ") lockTime is diffrent for block " << block_number;
+
+            auto vins = t.base()->get_vin();
+            int pos_v = 0;
+            for(auto vin = vins.begin(); vin < vins.end(); vin++) {
+                EXPECT_EQ(vin.base()->_sequence, j.at("tx")[pos].at("vin")[pos_v].at("sequence"))
+                                    << "Transaction (" << pos << ") vin (" << pos_v << ") is diffrent for block " << block_number;
+                pos_v++;
+            }
+            EXPECT_EQ(pos_v, t.base()->get_vin_count()) << "Transaction (" << pos << ") vin count is diffrent for block " << block_number;
+
+            auto vouts = t.base()->get_vout();
+            pos_v = 0;
+            for(auto vout = vouts.begin(); vout < vouts.end(); vout++) {
+//                //convert to int64_t
+//                EXPECT_EQ(vout.base()->_amount, j.at("tx")[pos].at("vout")[pos_v].at("n"));
+                pos_v++;
+            }
+            EXPECT_EQ(pos_v, t.base()->get_vout_count()) << "Transaction (" << pos << ") vout count is diffrent for block " << block_number;
+            pos++;
+        }
+        EXPECT_EQ(pos, b.get_number_of_transactions()) << "Transaction number is different for block " << block_number;
+    }
+
 }
 
 TEST(parsing, correct_block_22380_json) {
     compare_bin_json(22380);
+}
+
+TEST(parsing, correct_block_22381_json) {
+    compare_bin_json(22381);
+}
+
+TEST(parsing, correct_block_22382_json) {
+    compare_bin_json(22382);
+}
+
+TEST(parsing, correct_block_22383_json) {
+    compare_bin_json(22383);
+}
+
+TEST(parsing, correct_block_22384_json) {
+    compare_bin_json(22384);
+}
+
+TEST(parsing, correct_block_22385_json) {
+    compare_bin_json(22385);
+}
+
+TEST(parsing, correct_block_22386_json) {
+    compare_bin_json(22386);
+}
+
+TEST(parsing, correct_block_22387_json) {
+    compare_bin_json(22387);
+}
+
+TEST(parsing, correct_block_22388_json) {
+    compare_bin_json(22388);
+}
+
+TEST(parsing, correct_block_22389_json) {
+    compare_bin_json(22389);
+}
+
+TEST(parsing, correct_block_22390_json) {
+    compare_bin_json(22390);
 }
